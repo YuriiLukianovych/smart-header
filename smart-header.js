bf88410cbd2dc858b2d8311d209e032a22bea65b
Yurii Lukianovych <mr.lukyanovich@gmail.com>
2023-10-10 15:24:55 +0300

    added layout

--- smart-header.js
+++ smart-header.js
@@ -1,21 +1,23 @@
-export const smartHeader = () => {
-    let lastScroll = 0;
-    const header = document.querySelector('.header');
-    const defaultOffset = 100;
+let lastScroll = 0;
+const header = document.querySelector(".header");
+const defaultOffset = 100;
 
-    const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
-    const isHide = () => header.classList.contains('hide');
+const scrollPosition = () =>
+    window.scrollY || document.documentElement.scrollTop;
+const isHide = () => header.classList.contains("hide");
 
-    window.addEventListener('scroll', () => {
-        
-        if (scrollPosition() > lastScroll && !isHide() && scrollPosition() > defaultOffset) {
-            // scroll down
-            header.classList.add('hide');
-        } else if (scrollPosition() < lastScroll && isHide()) {
-            // scroll up
-            header.classList.remove('hide');
-        }
-        
-        lastScroll = scrollPosition();
-    })
-}
+window.addEventListener("scroll", () => {
+    if (
+        scrollPosition() > lastScroll &&
+        !isHide() &&
+        scrollPosition() > defaultOffset
+    ) {
+        // scroll down
+        header.classList.add("hide");
+    } else if (scrollPosition() < lastScroll && isHide()) {
+        // scroll up
+        header.classList.remove("hide");
+    }
+
+    lastScroll = scrollPosition();
+});
